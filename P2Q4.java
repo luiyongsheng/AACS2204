0de3247e25b65c0db0bf0d4a8cbd33e484efa80e
Lui Yong Sheng <sheng_9707@live.com>
2017-06-16 15:22:23 +0800

    Much precise algorithm

--- P2Q4.java
+++ P2Q4.java
@@ -4,11 +4,12 @@ class altMath {
     public static double sqrt(double x) {
         if(x == 0) return 0;
         else if (x < 0) { return -1; } // negative value as error indicator as java does not contain nullable type
+        x = Math.abs(x);
         double low, high, mid = 0;
         low = Math.min(1,x);
         high = Math.max(1,x);
         // precision
-        while(!((high - low) / low <= epsilon)) {
+        while(!(((high - low) / low) <= epsilon)) {
             mid = (low + high) / 2;
             if((mid * mid) <= x) {
                 low = mid;
@@ -17,10 +18,21 @@ class altMath {
         }
         return low;
     }
+    public static double newtonSqrt(double x) {
+        if(x < 0) return Double.NaN;
+        double t = x;
+
+        while(Math.abs(t - x/t) > epsilon * t) 
+            t = (x/t + t) / 2.0;
+        
+        return t;
+    }
 }
 
 public class P2Q4 {
     public static void main(String[] args) {
-        System.out.println(altMath.sqrt(2));
+        for(double i = 0; i < 20; i += 0.5) {
+            System.out.println("sqrt("+ i +") = " + altMath.newtonSqrt(i));
+        }
     }
 }
