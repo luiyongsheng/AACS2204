936917760bb0916e3f26c9aa02c467d9d3961d40
luiyongsheng <sheng_9707@live.com>
2017-06-22 22:09:48 +0800

    Update

--- P4Q1c.java
+++ P4Q1c.java
@@ -1,16 +1,18 @@
 class EmployeeX {
     String name;
     double salary;
-    void EmployeeX(String employeeName, double currentSalary) {
+    EmployeeX(String employeeName, double currentSalary) {
         name = employeeName;
         salary = currentSalary;
     }
     EmployeeX(String employeeName) {
-        this.EmployeeX(employeeName, 0.00);
+        name = employeeName;
+        salary = 0.00;
     }
 
     EmployeeX() {
-        this.EmployeeX("", 0.00);
+        name = "";
+        salary = 0.00;
     }
     void raiseSalary(double percent) {
         salary = salary + (salary * percent / 100.0);
@@ -19,6 +21,18 @@ class EmployeeX {
 }
 public class P4Q1c {
     public static void main(String[] args) {
-
+        EmployeeX[] exs = new EmployeeX[2];
+        exs[0] = new EmployeeX("Lui Yong Sheng", 9000);
+        exs[1] = new EmployeeX("James Gosling", 12583);
+        double maxPoint = 0; int highIndex = 0;
+        for(int i = 0; i < exs.length; i++) {
+            double tmp = maxPoint;
+            maxPoint = Math.max(maxPoint, exs[i].salary);
+            if(maxPoint != tmp) {
+                highIndex = i;
+            }
+        }
+        System.out.format("%s has the highest salary which is $%.2f\n",
+                         exs[highIndex].name, exs[highIndex].salary);
     }
 }
