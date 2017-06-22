e05f4235a4d17b9559e4ae9a6f1de0f2867caaef
luiyongsheng <sheng_9707@live.com>
2017-06-22 16:24:23 +0800

    Update

--- P4Q1.java
+++ P4Q1.java
@@ -13,6 +13,12 @@ class Employee{
 }
 public class P4Q1 {
     public static void main(String[] args) {
+        Employee emp = new Employee("Lui Yong Sheng", 97803.30);
+        System.out.format("%s's salary before 8%% salary raise : RM %.2f\n",
+                         emp.name, emp.salary);
+        emp.raiseSalary(8);
         
+        System.out.format("%s's salary after 8%% salary raise : RM %.2f\n",
+                         emp.name, emp.salary);
     }
 }
