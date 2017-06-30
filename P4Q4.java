93cd48caf4ce6fffbbfc14c31ccbfa74c1414e33
Lui Yong Sheng <sheng_9707@live.com>
2017-06-30 14:44:01 +0800

    Create P4Q4.java

---
+++ P4Q4.java
@@ -0,0 +1,42 @@
+class Course {
+    private String courseTitle;
+    public String getCourseTitle() {
+        return this.courseTitle;
+    }
+    public void setCourseTitle(String val) {
+        this.courseTitle = val;
+    }
+
+    private double feesPerStudent;
+    public double getFeesPerStudent() {
+        return this.feesPerStudent;
+    }
+    public void setFeesPerStudent(double val) {
+        this.feesPerStudent = val;
+    }
+
+    private int noOfStudents;
+    public int getNoOfStudents() {
+        return this.noOfStudents;
+    }
+
+    private String[] studentNames;
+
+    private int courseCount;
+    public int getCourseCount() {
+        return this.courseCount;
+    }
+
+    Course(String courseTitle, double feesPerStudent) {
+
+    }
+    
+
+}
+
+
+public class P4Q4 {
+    public static void main(String[] args) {
+
+    }
+}
