fd00397da8420b86d761c881ca0c1225fc5dc88e
Lui Yong Sheng <sheng_9707@live.com>
2017-06-30 14:53:50 +0800

    Update P4Q4.java

--- P4Q4.java
+++ P4Q4.java
@@ -28,15 +28,33 @@ class Course {
     }
 
     Course(String courseTitle, double feesPerStudent) {
-
+        this.courseTitle = courseTitle;
+        this.feesPerStudent = feesPerStudent;
+        this.studentNames = new String[100];
     }
     
+    public double calcFeesCollected() {
+        return this.feesPerStudent * this.noOfStudents;
+    }
 
+    public void addStudent(String studName) {
+        if(this.noOfStudents == 100) return;
+        else {
+            this.studentNames[this.noOfStudents] = studName;
+        }
+    }
+    public String toString() {
+        return "Course Title : " + this.courseTitle + "\nFee Per Student : " + this.feesPerStudent + 
+               "\nNo of Student : " + this.noOfStudents; 
+    }
 }
 
 
 public class P4Q4 {
     public static void main(String[] args) {
+        Course course = new Course("Introduction to Computers", 250);
+        System.out.format("Course Title     : %s\n", course.getCourseTitle());
+        System.out.format("Fees per student : RM %.2f\n", course.getFeesPerStudent());
 
     }
 }
